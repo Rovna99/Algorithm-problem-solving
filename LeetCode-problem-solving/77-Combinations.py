2099686a41fdc87bef1eff749d2aa6ddf8780906
Rovna99 <tjdalswodi91@gmail.com>
2021-08-25 18:45:54 +0900

    today study

---
+++ LeetCode-problem-solving/77-Combinations.py
@@ -0,0 +1,23 @@
+import itertools
+
+
+class Solution:
+    #DFS
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        results = []
+
+        def dfs(elements, start: int, k: int):
+            if k == 0:
+                results.append(elements[:])
+                return
+
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k - 1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return results
+    #itertools
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(range(1, n+1), k))
