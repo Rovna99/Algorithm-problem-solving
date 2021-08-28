d7d3e1ade7b26cd9ce9bb3556d74991f1f25e43d
Rovna99 <tjdalswodi91@gmail.com>
2021-08-28 15:37:48 +0900

    .

--- BOJ-problem-solving/1085-직사각형에서 탈출.py
+++ BOJ-problem-solving/1085-직사각형에서 탈출.py
@@ -1,13 +1,3 @@
-while True:
-    num = list(map(int, input().split()))
-    x = min(num)
-    z = max(num)
-    num.remove(x)
-    num.remove(z)
-    y = num[0]
-    if x == 0 and y == 0 and z == 0:
-        break
-    if x ** 2 + y ** 2 == z ** 2:
-        print('right')
-    else:
-        print('wrong')
+x, y, w, h = map(int, input().split())
+
+print(min(h - y, w - x, x, y))
