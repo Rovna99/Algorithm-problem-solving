b3cea76acdc02bbac8087112b4d915613d0ff7c2
Rovna99 <tjdalswodi91@gmail.com>
2021-08-31 21:53:06 +0900

    .

---
+++ LeetCode-problem-solving/148-sort-list.py
@@ -0,0 +1,38 @@
+#1 실무에서 사용할 만한 실용적인 풀이
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p = head
+        lst: List = []
+        while p:
+            lst.append(p.val)
+            p = p.next
+
+        lst.sort()
+
+        p = head
+        for i in range(len(lst)):
+            p.val = lst[i]
+            p = p.next
+        return head
+#2 병합정렬 이용
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1 or l2
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not (head and head.next):
+            return head
+
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+
+        return self.mergeTwoLists(l1, l2)
