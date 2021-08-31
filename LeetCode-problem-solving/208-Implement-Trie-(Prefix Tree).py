5ed5eaf6fc9826f24e2987751f69fccedd8ebae7
Rovna99 <tjdalswodi91@gmail.com>
2021-08-31 20:50:56 +0900

    finish

---
+++ LeetCode-problem-solving/208-Implement-Trie-(Prefix Tree).py
@@ -0,0 +1,46 @@
+import collections
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+
+        return True
--- LeetCode-problem-solving/215. Kth Largest Element in an Array.py
+++ LeetCode-problem-solving/215. Kth Largest Element in an Array.py
@@ -10,3 +10,23 @@ class Solution:
             heapq.heappop(heap)
 
         return -heapq.heappop(heap)
+
+#2 heapq 모듈의 heapify 이용
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
+
+        return heapq.heappop(nums)
+
+#3 heapq 모듈의 nlarges 이용
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[-1]
+
+#4 정렬을 이용한 풀이
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return sorted(nums, reverse=True)[k -1]
