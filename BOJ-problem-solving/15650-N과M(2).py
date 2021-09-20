e4e7dbfb1f154d5df51741ad54574abcdab906cb
Rovna99 <tjdalswodi91@gmail.com>
2021-09-20 16:43:52 +0900

    Update 15650-N과M(2).py

--- BOJ-problem-solving/15650-N과M(2).py
+++ BOJ-problem-solving/15650-N과M(2).py
@@ -4,11 +4,12 @@ result = []
 def NaM(s):
     if len(result) == M:
         print(' '.join(map(str, result)))
+        return
 
     for i in range(s, N+1):
         if i not in result:
             result.append(i)
-            NaM(s+1)
+            NaM(i+1)
             result.pop()
 
 
