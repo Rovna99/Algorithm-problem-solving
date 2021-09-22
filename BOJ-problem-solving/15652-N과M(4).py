0fd1ccaadbbdb290350177f9ac82052aa7684d92
Rovna99 <tjdalswodi91@gmail.com>
2021-09-22 09:35:42 +0900

    Update 15652-N과M(4).py

--- BOJ-problem-solving/15652-N과M(4).py
+++ BOJ-problem-solving/15652-N과M(4).py
@@ -1,1 +1,19 @@
+N, M = map(int, input().split())
+result = []
 
+
+def NaM(s):
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+
+    for i in range(1, N+1):
+        if len(result) > 0 and result[0] > i:
+            continue
+        else:
+            result.append(i)
+            NaM(i + 1)
+            result.pop()
+
+
+NaM(1)
