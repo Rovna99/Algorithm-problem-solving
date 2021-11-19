98f1bd2d31be86e7faebc67d1c3d3cdb083a58e9
Rovna99 <tjdalswodi91@gmail.com>
2021-11-19 18:45:49 +0900

    Update 4673-셀프-넘버.py

--- BOJ-problem-solving/브론즈/4673-셀프-넘버.py
+++ BOJ-problem-solving/브론즈/4673-셀프-넘버.py
@@ -1,9 +1,10 @@
-result = set(range(1, 10001))
-del_num = set()
+result = set(range(1, 10001))  # 범위 지정
+del_num = set()  # 생성자가 있는 수들을 담을 set
 for n in result:
-    for i in str(n):
-        n += int(i)
+    for i in str(n):  # 숫자의 한 자릿수씩
+        n += int(i)  # n에다 더해준다.
     del_num.add(n)
-result -= del_num
+result -= del_num  # 생성자가 있는 수들 제거
 for i in sorted(result):
     print(i)
+    
